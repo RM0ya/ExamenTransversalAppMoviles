ca6059fb7c2388422dc61f6e7b262a5511e28dfc
RodrigöMoya <r.moya@duocuc.cl>
2025-12-12 10:37:44 -0300

    Update package declaration in CarritoViewModelTest

--- app/src/test/java/com/example/pasteleriakotlin/JunitTest/CarritoViewModelTest.kt
+++ app/src/test/java/com/example/pasteleriakotlin/JunitTest/CarritoViewModelTest.kt
@@ -1,4 +1,4 @@
-package com.example.pasteleriakotlin.JunitTest
+package com.example.pasteleriakotlin
 
 import com.example.pasteleriakotlin.model.Product
 import com.example.pasteleriakotlin.viewmodel.CarritoViewModel
